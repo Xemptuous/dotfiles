62c546a63c0660efbb750b696b8947a677d92d08
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2024-09-12 21:12:29 -0700

    updated nightmode script

--- nightmode.sh
+++ nightmode.sh
@@ -15,10 +15,9 @@ if [[ "$1" = "on" || $1 = 1 ]]; then
     sed -i 's/latte/mocha/' ~/.config/sway/config 2&>/dev/null
     sed -i 's/latte/mocha/' ~/.config/helix/config.toml 2&>/dev/null
     sed -i 's/latte/mocha/' ~/.config/zellij/config.kdl 2&>/dev/null
-    swaymsg reload
     echo "y" | fish -c "fish_config theme save 'Catppuccin Mocha'" 2&>/dev/null
     export BAT_THEME='Catppuccin Mocha'
-    export LS_COLORS="$(vivid generate catppuccin-mocha)"
+    # export LS_COLORS="$(vivid generate catppuccin-mocha)"
 
 else
     sed -i 's/mocha/latte/' ~/.config/fish/config.fish 2&>/dev/null
@@ -30,8 +29,9 @@ else
     sed -i 's/mocha/latte/' ~/.config/sway/config 2&>/dev/null
     sed -i 's/mocha/latte/' ~/.config/helix/config.toml 2&>/dev/null
     sed -i 's/mocha/latte/' ~/.config/zellij/config.kdl 2&>/dev/null
-    swaymsg reload
     echo "y" | fish -c "fish_config theme save 'Catppuccin Latte'" 2&>/dev/null
     export BAT_THEME='Catppuccin Latte'
-    export LS_COLORS="$(vivid generate catppuccin-latte)"
+    # export LS_COLORS="$(vivid generate catppuccin-latte)"
 fi
+swaymsg reload 2&>/dev/null
+fish -c "set --global hydro_color_git $fish_color_host_remote" 2&>/dev/null
